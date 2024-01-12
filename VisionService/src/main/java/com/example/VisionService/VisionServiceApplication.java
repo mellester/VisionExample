866fbe283d68be372cd7deda5973bbe1588c63f1
Krste Vrkic <krste.vrkic@gmail.com>
2024-01-12 10:11:29 +0100

    Added springboot controller

---
+++ VisionService/src/main/java/com/example/VisionService/VisionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.VisionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionServiceApplication.class, args);
+	}
+
+}
---
+++ VisionService/src/test/java/com/example/VisionService/VisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VisionService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
