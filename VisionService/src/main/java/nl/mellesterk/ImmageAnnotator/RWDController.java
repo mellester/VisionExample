65459a7c0670cf2fe8050a64f0df08efdb06bbb1
melle <mellester1@gmail.com>
2024-02-29 14:11:30 +0100

    Made a small change

--- VisionService/src/main/java/nl/mellesterk/ImmageAnnotator/RWDController.java
+++ VisionService/src/main/java/nl/mellesterk/ImmageAnnotator/RWDController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Pattern;
 
 import java.io.BufferedReader;
