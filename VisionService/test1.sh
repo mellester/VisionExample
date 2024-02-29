d3d230e7aa405eed20f28ab4720a2740753bc58c
melle <mellester1@gmail.com>
2024-02-29 13:58:23 +0100

    Working website

---
+++ VisionService/test1.sh
@@ -0,0 +1,10 @@
+curl 'https://ovi.rdw.nl/default.aspx' \
+  -H 'sec-ch-ua: "Not A(Brand";v="99", "Google Chrome";v="121", "Chromium";v="121"' \
+  -H 'Accept: text/html, */*; q=0.01' \
+  -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' \
+  -H 'Referer: https://ovi.rdw.nl/default.aspx' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  --data-raw '__VIEWSTATE=%2FwEPDwUKMTE1NDI3MDEyOQ9kFgJmD2QWAgIDD2QWAgIBD2QWAgIJDxYCHgdWaXNpYmxlaGRkbswJ9j6e1dGi3igmthIyvj8gc4WH7JTnjgF14yLn50Y%3D&__VIEWSTATEGENERATOR=CA0B0334&__EVENTVALIDATION=%2FwEdAAK8JKqL%2BRK4NWrMB2xu8rTB851Fq81QBiZgFEttEk2eeHewUOoCJ2ceZf9ZQoBBMXI2OTBKyEO6GXbPjhMnQTm0&ctl00%24TopContent%24txtKenteken=X-737-GX' \
+  --compressed
